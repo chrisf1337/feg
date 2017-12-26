fb3c9fb86ca9261032a444029ab54f309ac3cc44
Christopher Fu <chrisf1337@gmail.com>
2017-12-26 02:28:21 -0800

    Draw sprite in grid

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
+#![feature(use_nested_groups)]
 extern crate ggez;
 
 use ggez::*;
 use ggez::event::*;
-use ggez::graphics::{DrawMode, Point2};
+use ggez::graphics::{Drawable, DrawMode, Point2, DrawParam, Rect, Image};
 use ggez::conf::{WindowMode, WindowSetup};
 use std::env;
 use std::path;
 
-fn world_to_screen_coords(screen_width: u32, screen_height: u32, point: Point2) -> Point2 {
-    let width = screen_width as f32;
-    let height = screen_height as f32;
-    let x = point.x + width / 2.0;
-    let y = height - (point.y + height / 2.0);
-    Point2::new(x, y)
-}
-
 struct MainState {
     mouse_coords: (u32, u32),
-    font: graphics::Font
+    font: graphics::Font,
+    spritesheet: Image
 }
 
 const WINDOW_WIDTH: u32 = 1280;
@@ -33,10 +27,10 @@ impl MainState {
     fn new(ctx: &mut Context) -> GameResult<Self> {
         Ok(MainState {
             mouse_coords: (0, 0),
-            font: graphics::Font::new(ctx, "/DejaVuSerif.ttf", 10)?
+            font: graphics::Font::new(ctx, "/DejaVuSerif.ttf", 10)?,
+            spritesheet: Image::new(ctx, "/celica-1.png")?,
         })
     }
-
 }
 
 fn draw_grid(ctx: &mut Context) -> GameResult<()> {
@@ -63,6 +57,7 @@ fn screen_to_grid_coord((screen_x, screen_y): (u32, u32)) -> Option<(u32, u32)> 
     Some(((screen_x - HORIZONTAL_PADDING) / GRID_CELL_DIM, (screen_y - VERTICAL_PADDING) / GRID_CELL_DIM))
 }
 
+// Screen coord is the top left hand corner of the cell, not including line width
 fn grid_to_screen_coord((grid_x, grid_y): (u32, u32)) -> (u32, u32) {
     (HORIZONTAL_PADDING + grid_x * GRID_CELL_DIM + GRID_LINE_WIDTH / 2,
      VERTICAL_PADDING + grid_y * GRID_CELL_DIM + GRID_LINE_WIDTH / 2)
@@ -78,21 +73,28 @@ impl event::EventHandler for MainState {
         draw_grid(ctx)?;
         let fps = (ggez::timer::get_fps(ctx) as u32).to_string();
         let fps_txt = graphics::Text::new(ctx, &fps, &self.font)?;
-        graphics::draw(ctx, &fps_txt, Point2::new(WINDOW_WIDTH as f32 - 40.0, 20.0), 0.0)?;
+        fps_txt.draw(ctx, Point2::new(WINDOW_WIDTH as f32 - 40.0, 20.0), 0.0)?;
         match screen_to_grid_coord(self.mouse_coords) {
             Some((grid_x, grid_y)) => {
                 let (rect_x, rect_y) = grid_to_screen_coord((grid_x, grid_y));
                 let old_color = graphics::get_color(ctx);
-                graphics::set_color(ctx, graphics::Color::from_rgba(255, 255, 255, 100))?;
+                graphics::set_color(ctx, graphics::Color::from_rgb(234, 152, 174))?;
                 graphics::rectangle(ctx,
                                     DrawMode::Fill,
-                                    graphics::Rect::new(rect_x as f32, rect_y as f32,
-                                                        (GRID_CELL_DIM - GRID_LINE_WIDTH) as f32,
-                                                        (GRID_CELL_DIM - GRID_LINE_WIDTH) as f32))?;
-                graphics::set_color(ctx, old_color);
+                                    Rect::new(rect_x as f32, rect_y as f32,
+                                              (GRID_CELL_DIM - GRID_LINE_WIDTH) as f32,
+                                              (GRID_CELL_DIM - GRID_LINE_WIDTH) as f32))?;
+                graphics::set_color(ctx, old_color)?;
             }
             None => ()
         }
+        let screen_coord = grid_to_screen_coord((0, 0));
+        self.spritesheet.draw_ex(ctx, DrawParam {
+            src: Rect::new(0.0, 0.0, 1.0 / 4.0, 1.0),
+            dest: Point2::new(screen_coord.0 as f32, screen_coord.1 as f32),
+            scale: Point2::new(2.0, 2.0),
+            ..DrawParam::default()
+        })?;
         graphics::present(ctx);
         Ok(())
     }
